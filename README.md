27193ff04823c8f0393aaec5ff6bb61e4fa632c1
ShrRa <shr.razim@gmail.com>
2024-12-06 17:53:08 +0100

    Update readme and project structure

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Introduction
 
-This is a template software project repository used by the [Intermediate Python for Astronomical Software Development](https://shrra.github.io/python-intermediate-development/index.html).
+This is a template software project repository used by the [Single-Day Intermediate Python for Astronomical Software Development: Testing](https://shrra.github.io/interpython_1day_testing/00-setting-the-scene/index.html) workshop.
 
 ## Purpose
 
-This repository is intended to be used as a code template which is copied by learners at [Intermediate Python for Astronomical Software Development](https://shrra.github.io/python-intermediate-development/index.html) course.
+This repository is intended to be used as a code template which is copied by learners at [Single-Day Intermediate Python for Astronomical Software Development: Testing](https://shrra.github.io/interpython_1day_testing/00-setting-the-scene/index.html) course.
 This can be done using the `Fork` button towards the top right of this repo's GitHub page.
 
 This software project is not finished, is currently failing to run and contains some code style issues. It is used as a starting point for the course - issues will be fixed and code will be added in a number of places during the course by learners in their own copies of the repository, as course topics are introduced.
--- lcanalyzer/models.py
+++ lcanalyzer/models.py
@@ -3,14 +3,13 @@
 The Model layer is responsible for the 'business logic' part of the software.
 
 The lightcurves are saved in a table (2D array) where each row corresponds to a single observation. 
-Depending on the dataset (LSST or Kepler), a table can contain observations 
-of a single or several objects, 
+Depending on the dataset (LSST or Kepler), a table can contain observations of a single or several objects, 
 in a single or different bands.
 """
 
 import pandas as pd
-#import numpy as np
-#from astropy.timeseries import LombScargle
+import numpy as np
+from astropy.timeseries import LombScargle
 
 def load_dataset(filename):
     """Load a table from CSV file.
@@ -22,20 +21,12 @@ def load_dataset(filename):
 
 
 def mean_mag(data,mag_col):
-    """Calculate the mean magnitude of a lightcurve
-    :param data: pd.DataFrame with observed magnitudes for a single source.
-    :param mag_col: a string with the name of the column for calculating the mean.
-    :returns: A float with the mean value of the column.
-    """
+    """Calculate the mean magnitude of a lightcurve"""
     return data[mag_col].mean()
 
 
 def max_mag(data,mag_col):
-    """Calculate the max magnitude of a lightcurve
-    :param data: pd.DataFrame with observed magnitudes for a single source.
-    :param mag_col: a string with the name of the column for calculating the max value.
-    :returns: The max value of the column.
-    """
+    """Calculate the max magnitude of a lightcurve"""
     return data[mag_col].max()
 
 
@@ -46,11 +37,3 @@ def min_mag(data,mag_col):
     :returns: The min value of the column.
     """
     return data[mag_col].min()
-
-def calc_stat(lc, bands, mag_col):
-    # Define an empty dictionary where we will store the results
-    stat = {}
-    # For each band get the maximum value and store it in the dictionary
-    for b in bands:
-        stat[b + "_max"] = max_mag(lc[b], mag_col)
-    return stat
